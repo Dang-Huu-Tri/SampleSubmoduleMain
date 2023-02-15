64adeb109360d001b24112fab2ee22775cb5733e
ben.dang <ben.dang@geocomply.com>
2023-02-15 10:35:34 +0700

    add submodules

--- SampleSubmoduleMain/ViewController.swift
+++ SampleSubmoduleMain/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import SampleSubmodulesProject
+import SampleDefaultProject
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let aa = SampleSubmodulesClass()
+        print(aa.sampleString)
+        
+        let testaa = TestDefaultA()
+        print(testaa.testVariable)
         // Do any additional setup after loading the view.
     }
 
 
 }
+
 
